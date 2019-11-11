1dc7cad35e1e81e68e47c1ada023b81ad2e63427
saquilabon <arnelsaquilabon@gmail.com>
2019-11-12 01:21:30 +0800

    User live server url.

--- selenium_test/tests.py
+++ selenium_test/tests.py
@@ -14,7 +14,7 @@ class SeleniumTest(StaticLiveServerTestCase):
     def test_login(self):
         user_name = "arnelsaquilabon@gmail.com"
         password = "password"
-        self.browser.get("http://localhost:8000/login/")
+        self.browser.get(self.live_server_url + "/login/")
         self.browser.find_element_by_id("username").send_keys(user_name)
         self.browser.find_element_by_id("password").send_keys(password)
         self.browser.find_element_by_class_name("btn").click()
