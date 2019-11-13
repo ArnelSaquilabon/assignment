a2c1dfbe1335f22fed8dbd8d0e5ed1533e7f50c5
saquilabon <arnelsaquilabon@gmail.com>
2019-11-14 01:11:00 +0800

    Update model to fix server issue when running.

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,4 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
-    password = models.TextField()
-    salt = models.CharField(max_length=100)
-    date_registered = models.DateTimeField()
+    password = models.CharField(max_length=50)
