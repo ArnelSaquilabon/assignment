8dc9d7c3997168f2b0cd272bd5e1e71f0cf6f9a8
saquilabon <arnelsaquilabon@gmail.com>
2019-11-17 03:58:25 +0800

    Implement azure pipeline and update selenium test.

--- selenium_test/tests.py
+++ selenium_test/tests.py
@@ -1,11 +1,22 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+import platform
+import os
 
 
 class SeleniumTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome("selenium_test/drivers/chromedriver.exe")
+        directory = os.getcwd()
+        chrome_driver_name = 'chromedriver' if platform.system() == 'Linux' else 'chromedriver.exe'
+        chrome_driver_directory = directory + "/selenium_test/drivers/" + chrome_driver_name
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-extensions')
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        self.browser = webdriver.Chrome(chrome_options=options,
+                                        executable_path=chrome_driver_directory)
         self.browser.set_page_load_timeout(10)
 
     def tearDown(self):
